5977ddb238876c3648af312b1014c114a23048ac
Serggi0 <serve4mia@gmail.com>
2021-06-10 12:53:49 +0300

    assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,11 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello', "Houston we've got a problem"
+# if capitalize('hello') != 'Hello':
+#     raise Exception('Функция работает неверно!')
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+# if capitalize('') != '':
+#     raise Exception('Функция работает неверно!')
 
-print('Все тесты пройдены!')
+# print('Все тесты пройдены!')
